84c48fcde3a199e6b00997a31e04732e126e27da
Ruban Sahoo <ruban@192.168.1.5>
2023-07-11 09:35:39 +0530

    added HTML, CSS and Go intro

---
+++ Go/HELLOWORLD/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+ func main() {
+	fmt.Println("Hi there!")
+ }
+
+// Inside the project folder you can rin the following commands:
+// To run the code: `go run main.go`
+// To compile the code and generate an executable: `go build main.go`
